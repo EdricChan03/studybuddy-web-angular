d49c9a7d06b43f515bb9afc76d1499b2d70d9fec
Edric Chan <edric.chan.1997@gmail.com>
2017-11-05 12:54:23 +0800

    fix: Use seperate endpoints for Material modules

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,48 +1,58 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatNativeDateModule } from '@angular/material/core';
 import {
-	MdButtonModule,
-	MdToolbarModule,
-	MdSidenavModule,
-	MdIconModule,
-	MdListModule,
-	MdMenuModule,
-	MdCardModule,
-	MdFormFieldModule,
-	MdInputModule,
-	MdDatepickerModule,
-	MdNativeDateModule,
-	MdSnackBarModule,
-	MdTooltipModule,
-	MdDialogModule,
-	MdRadioModule,
-	MdSelectModule,
-    MdCheckboxModule
-} from '@angular/material';
+	CdkTableModule
+} from '@angular/cdk/table';
 const MATERIAL_MODULES = [
-	MdButtonModule,
-	MdToolbarModule,
-	MdSidenavModule,
-	MdIconModule,
-	MdListModule,
-	MdMenuModule,
-	MdCardModule,
-	MdFormFieldModule,
-	MdInputModule,
-	MdDatepickerModule,
-	MdNativeDateModule,
-	MdSnackBarModule,
-	MdTooltipModule,
-	MdDialogModule,
-	MdRadioModule,
-	MdSelectModule,
-	MdCheckboxModule
+	MatButtonModule,
+	MatToolbarModule,
+	MatSidenavModule,
+	MatIconModule,
+	MatListModule,
+	MatMenuModule,
+	MatCardModule,
+	MatFormFieldModule,
+	MatInputModule,
+	MatDatepickerModule,
+	MatNativeDateModule,
+	MatSnackBarModule,
+	MatTooltipModule,
+	MatDialogModule,
+	MatRadioModule,
+	MatSelectModule,
+	MatCheckboxModule,
+	MatTableModule,
+	MatPaginatorModule
 ];
+const CDK_MODULES = [
+	CdkTableModule
+]
 @NgModule({
 	imports: [
-		MATERIAL_MODULES
+		MATERIAL_MODULES,
+		CDK_MODULES
 	],
 	exports: [
-		MATERIAL_MODULES
+		MATERIAL_MODULES,
+		CDK_MODULES
 	]
 })
 export class MyMaterialModule { }
