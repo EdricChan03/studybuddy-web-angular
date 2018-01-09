a3d7b6c5a39d3891b5db0d25b14cab7ea568eded
Edric Chan <edric.chan.1997@gmail.com>
2018-01-10 00:30:26 +0800

    chore(routing): add routing for settings

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,13 +5,15 @@ import { AppdownloadsComponent } from './appdownloads/appdownloads.component';
 import { AppComponent } from './app.component';
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
+import { SettingsComponent } from './settings/settings.component';
 
 export const AppRoutes: Routes = [
 	{ path: 'downloads', component: AppdownloadsComponent },
 	{ path: 'todo', component: TodoComponent },
 	{ path: 'test', component: TestpageComponent },
+	{ path: 'settings', component: SettingsComponent },
 	{ path: 'resources', component: ResourcesComponent },
 	{ path: '**', redirectTo: 'todo' }
-]
+];
 
 export const AppRouting: ModuleWithProviders = RouterModule.forRoot(AppRoutes);
