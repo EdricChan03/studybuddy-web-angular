22b8a43e80295b5a2e50cba7c60e6b471037b627
Edric Chan <edric.chan.1997@gmail.com>
2018-01-10 00:32:10 +0800

    chore(todo): update syntax

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,11 +1,11 @@
-import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { TodoService, TodoDatabase, TodoDataSource } from './todo.service';
 import { Observable } from 'rxjs/Observable';
-import { NewTodoDialog } from './../newtodo/newtodo.component';
+import { NewTodoDialog } from '../newtodo/newtodo.component';
 import { MatDialog, MatPaginator } from '@angular/material';
-import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { Shared, Todo } from './../shared';
+import { SharedService } from '../shared.service';
+import { TodoItem } from '../interfaces';
 import { Component, OnInit, AfterViewInit, AfterContentInit, ViewChild } from '@angular/core';
 import { DataSource } from '@angular/cdk/collections';
 import 'rxjs/add/observable/of';
@@ -14,10 +14,11 @@ import 'rxjs/add/observable/of';
 	selector: 'app-todo',
 	templateUrl: './todo.component.html'
 })
-export class TodoComponent implements OnInit{
+export class TodoComponent implements OnInit {
 	currentUser: string;
-	todos: Observable<any>;
-	todoTable: boolean = false;
+	todos$: Observable<TodoItem[]>;
+	todosCollection: AngularFirestoreCollection<TodoItem>;
+	todoTable = false;
 	public dataSource: TodoDataSource | null;
 	public displayedColumns = [
 		'title',
@@ -26,13 +27,12 @@ export class TodoComponent implements OnInit{
 		'tags',
 		'importance',
 		'type'
-	]
+	];
 	@ViewChild(MatPaginator) paginator: MatPaginator;
 	public dataLength: any;
 	constructor(
-		private shared: Shared,
+		private shared: SharedService,
 		private afAuth: AngularFireAuth,
-		private db: AngularFireDatabase,
 		private dialog: MatDialog,
 		private todoService: TodoService,
 		private todoDatabase: TodoDatabase,
@@ -42,16 +42,19 @@ export class TodoComponent implements OnInit{
 			if (user) {
 				console.log(user);
 				this.currentUser = user.uid;
-				this.todos = this.db.list(`users/${this.currentUser}/todo`).valueChanges();
+				this.todosCollection = this.fs.collection(`users/${this.currentUser}/todos`);
+				this.todos$ = this.todosCollection.snapshotChanges().map(actions => {
+					return actions.map(a => {
+						const data = a.payload.doc.data() as TodoItem;
+						const id = a.payload.doc.id;
+						return { id, ...data };
+					});
+				});
+				console.log(this.fs.collection(`users/${this.currentUser}/todos`).valueChanges());
 			} else {
+				console.warn('Current user doesn\'t exist yet!');
 			}
-		})
-	}
-	/**
-	 * @todo
-	 */
-	deleteMember() {
-
+		});
 	}
 	/**
 	 * @todo
@@ -59,35 +62,30 @@ export class TodoComponent implements OnInit{
 	goToDetailPage() {
 	}
 	newTodo() {
-		this.dialog.open(NewTodoDialog);
+		this.dialog.open(NewTodoDialog, { disableClose: true });
 	}
-	removeTodo(key: string) {
-		this.shared.openConfirmDialog({ msg: "Are you sure you want to delete the todo? Once deleted, it will be lost forever and cannot be retrieved again." }).afterClosed().subscribe(res => {
-			if (res == 'ok') {
-				// this.todos.delete(key).then(_ => {
-				// 	this.shared.openSnackBar({ msg: "Todo was removed", additionalOpts: { duration: 3000 } });
-				// })
+	removeTodo(id: string) {
+		// tslint:disable-next-line:max-line-length
+		this.shared.openConfirmDialog({ msg: 'Are you sure you want to delete the todo? Once deleted, it will be lost forever and cannot be retrieved again.' }).afterClosed().subscribe(res => {
+			if (res === 'ok') {
+				this.todosCollection.doc(id).delete().then(() => {
+					this.shared.openSnackBar({ msg: 'Todo was removed', additionalOpts: { duration: 3000 } });
+				});
 			} else {
-				this.shared.openSnackBarWithRef({ msg: "Todo was not deleted", action: "Undo", additionalOpts: { duration: 6000 } }).onAction().subscribe(_ => {
-					this.removeTodo(key);
-				})
+				// tslint:disable-next-line:max-line-length
+				this.shared.openSnackBar({ msg: 'Todo was not deleted', action: 'Undo', additionalOpts: { duration: 6000 } }).onAction().subscribe(() => {
+					this.removeTodo(id);
+				});
 			}
-		})
-	}
-	getTodos() {
-		let result = this.shared.getTodos();
-		result.subscribe(res => {
-			console.log(res);
-		})
-		// console.log(result);
+		});
 	}
 	ngOnInit() {
 		this.todoDatabase.getTodos().subscribe(data => {
 			console.log(data);
 			this.dataSource = new TodoDataSource(this.todoDatabase, this.paginator);
 			this.dataLength = data;
-		})
-		console.log(`Data source: ${this.dataSource}`)
+		});
+		console.log(`Data source: ${this.dataSource}`);
 	}
 
 }
