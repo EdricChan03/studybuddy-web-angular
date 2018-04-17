b96b7a76518bc66cfe6d650e43fb72f3f0a81f1b
Edric Chan <edric.chan.1997@gmail.com>
2018-04-18 00:26:07 +0800

    feat: add initial dashboard component

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoItem } from '../interfaces';
+
+interface Dashboard {
+	todos: TodoItem[];
+	notes: any;
+}
+@Component({
+	selector: 'app-dashboard',
+	templateUrl: './dashboard.component.html',
+	styles: []
+})
+export class DashboardComponent implements OnInit {
+
+	constructor() { }
+	dashboard: Dashboard = {
+		todos: [
+			{
+				title: 'Todo #1',
+				dueDate: new Date(),
+				tags: ['tag-1', 'my-tag'],
+				content: 'Content goes here'
+			},
+			{
+				title: 'Todo #2',
+				dueDate: new Date(),
+				tags: ['tag-1', 'my-tag'],
+				content: '# Content goes here'
+			 	+ '\n**Bold**'
+				+ '\n_Italics_'
+				+ '\n~Strikethrough~'
+				+ '\n**_Bold + Italics_**'
+				+ '\n> I\'m a cool blockquote! XD Check out the code for more info.'
+				+ '\n\n```css'
+				+ '\n@import \'~@angular/material/prebuilt-themes/indigo-pink.css\''
+				+ '\n```'
+			},
+			{
+				title: 'Todo #3',
+				dueDate: new Date(),
+				tags: ['tag-1', 'my-tag'],
+				content: 'Content goes here'
+			},
+			{
+				title: 'Todo #4',
+				dueDate: new Date(),
+				tags: ['tag-1', 'my-tag'],
+				content: 'Content goes here'
+			}
+		],
+		notes: [
+			{
+
+			}
+		]
+	}
+	ngOnInit() {
+	}
+
+}
