d8a65c05dfcbc3728e8401fe1aa115f72da4c540
Edric Chan <edric.chan.1997@gmail.com>
2018-09-12 18:58:33 +0800

    chore(environment): add commenting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,14 @@
 export const environment = {
   production: true,
+  /**
+   * The Firebase config to be used with AngularFire2.
+   */
   firebase: {
-    apiKey: "AIzaSyBUoDJ_EL5ht9E4Dj9g5IjyjL4U8DTlyDU",
-    authDomain: "studybuddy-e5f46.firebaseapp.com",
-    databaseURL: "https://studybuddy-e5f46.firebaseio.com",
-    projectId: "studybuddy-e5f46",
-    storageBucket: "studybuddy-e5f46.appspot.com",
-    messagingSenderId: "713563449638"
+    apiKey: 'AIzaSyBUoDJ_EL5ht9E4Dj9g5IjyjL4U8DTlyDU',
+    authDomain: 'studybuddy-e5f46.firebaseapp.com',
+    databaseURL: 'https://studybuddy-e5f46.firebaseio.com',
+    projectId: 'studybuddy-e5f46',
+    storageBucket: 'studybuddy-e5f46.appspot.com',
+    messagingSenderId: '713563449638'
   }
 };
