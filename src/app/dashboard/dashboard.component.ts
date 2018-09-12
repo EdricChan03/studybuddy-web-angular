b2fe4e9d86d9751690c8b9d16f7e7becfbe222e2
Edric Chan <edric.chan.1997@gmail.com>
2018-09-12 18:57:08 +0800

    feat(dashboard): add initial support for viewing todos

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,65 +2,50 @@ import { Component, OnInit } from '@angular/core';
 import { TodoItem } from '../interfaces';
 import { SharedService } from '../shared.service';
 
-import * as firebase from 'firebase';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { MatDialog } from '@angular/material';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ToolbarService } from '../toolbar.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-interface Dashboard {
-  todos: TodoItem[];
-  notes: any;
-}
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html'
 })
-export class DashboardComponent implements OnInit {
-
+export class DashboardComponent {
+  currentUser: string;
+  todos$: Observable<TodoItem[]>;
+  todosCollection: AngularFirestoreCollection<TodoItem>;
   constructor(
-    private shared: SharedService
+    private shared: SharedService,
+    private afAuth: AngularFireAuth,
+    private dialog: MatDialog,
+    private afFs: AngularFirestore,
+    private dom: DomSanitizer,
+    public toolbarService: ToolbarService
   ) {
     shared.title = 'Dashboard';
-  }
-  dashboard: Dashboard = {
-    todos: [
-      {
-        title: 'Todo #1',
-        dueDate: firebase.firestore.Timestamp.now(),
-        tags: ['tag-1', 'my-tag'],
-        content: 'Content goes here'
-      },
-      {
-        title: 'Todo #2',
-        dueDate: firebase.firestore.Timestamp.now(),
-        tags: ['tag-1', 'my-tag'],
-        content: '# Content goes here'
-        + '\n**Bold**'
-        + '\n_Italics_'
-        + '\n~Strikethrough~'
-        + '\n**_Bold + Italics_**'
-        + '\n> I\'m a cool blockquote! XD Check out the code for more info.'
-        + '\n\n```css'
-        + '\n@import \'~@angular/material/prebuilt-themes/indigo-pink.css\''
-        + '\n```'
-      },
-      {
-        title: 'Todo #3',
-        dueDate: firebase.firestore.Timestamp.now(),
-        tags: ['tag-1', 'my-tag'],
-        content: 'Content goes here'
-      },
-      {
-        title: 'Todo #4',
-        dueDate: firebase.firestore.Timestamp.now(),
-        tags: ['tag-1', 'my-tag'],
-        content: 'Content goes here'
+    afAuth.auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.log(user);
+        this.currentUser = user.uid;
+        this.todosCollection = this.afFs.collection<TodoItem>(`users/${this.currentUser}/todos`);
+        this.todos$ = this.todosCollection.snapshotChanges().pipe(map(actions => {
+          return actions.map(a => {
+            const data = a.payload.doc.data() as TodoItem;
+            data.id = a.payload.doc.id;
+            return data;
+          });
+        }));
+        this.todos$.subscribe(() => {
+          this.toolbarService.setProgress(false);
+        });
+      } else {
+        console.warn('Current user doesn\'t exist yet!');
       }
-    ],
-    notes: [
-      {
-
-      }
-    ]
-  };
-  ngOnInit() {
+    });
   }
 
 }
