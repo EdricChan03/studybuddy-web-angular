dd30842a3381ec7e3e0d71386a2a93bad105c3f6
Edric Chan <edric.chan.1997@gmail.com>
2018-09-12 12:19:33 +0800

    chore: lint fixes

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -11,7 +11,7 @@ import * as firebase from 'firebase';
 export class AccountComponent {
 
   user: firebase.User;
-  isSignedIn: boolean = false;
+  isSignedIn = false;
   constructor(
     private shared: SharedService,
     private authService: AuthService,
@@ -31,13 +31,19 @@ export class AccountComponent {
     });
   }
   signOut() {
-    // tslint:disable-next-line:max-line-length
-    let dialogRef = this.shared.openConfirmDialog({ title: 'Log out?', msg: 'Changes not saved will be lost.', ok: 'Log out', okColor: 'warn' });
+    const dialogRef = this.shared.openConfirmDialog({
+      title: 'Log out?',
+      msg: 'Changes not saved will be lost.',
+      ok: 'Log out',
+      okColor: 'warn'
+    });
     dialogRef.afterClosed().subscribe(result => {
       if (result === 'ok') {
         this.authService.logOut().then((res) => {
-          // tslint:disable-next-line:max-line-length
-          const snackbarRef = this.shared.openSnackBar({ msg: 'Signed out', action: 'Undo', additionalOpts: { duration: 4000, horizontalPosition: 'start' } });
+          const snackbarRef = this.shared.openSnackBar({
+            msg: 'Signed out',
+            action: 'Undo'
+          });
           snackbarRef.onAction().subscribe(() => {
             this.newSignIn('google');
           });
@@ -46,22 +52,21 @@ export class AccountComponent {
           this.handleError(error.message);
         });
       }
-    })
+    });
   }
   /**
    * Signs in with Google
    */
   signInWithGoogle() {
     this.authService.logInWithGoogle().then((result) => {
-      // tslint:disable-next-line:max-line-length
-      this.shared.openSnackBar({ msg: `Signed in as ${result.user.email}`, additionalOpts: { duration: 4000, horizontalPosition: 'start', panelClass: 'mat-elevation-z3' } });
+      this.shared.openSnackBar({ msg: `Signed in as ${result.user.email}` });
     }).catch((error) => {
       this.handleError(error.message);
     });
   }
   /**
    * Uses new sign in
-   * @param {'google'|'anonymous'|'email'} authType The authentication type (optional, assumes default method is Google)
+   * @param authType The authentication type (optional, assumes default method is Google)
    */
   newSignIn(authType?: 'google' | 'anonymous' | 'email', params?: any) {
     // Checks if the authType argument is passed
@@ -84,83 +89,71 @@ export class AccountComponent {
       this.signInWithGoogle();
     }
   }
-  exportData() {
-    // tslint:disable-next-line:max-line-length
-    this.shared.openAlertDialog({ title: 'Export data', msg: '<p>Unfortunately, there\'s currently no such support for exporting/ importing data from/ to the database.</p><p>You should probably keep a physical copy of your notes somewhere! :)</p>', isHtml: true, ok: 'Dismiss' });
-  }
   deleteData() {
-    this.shared.openConfirmDialog({ title: 'Delete data?', msg: 'Once deleted, the data cannot be retrieved again!' }).afterClosed().subscribe(result => {
-      if (result == 'ok') {
-        this.shared.openSnackBar({ msg: 'We\'re currently deleting your data. Hold up tight! This may take a while.', additionalOpts: { horizontalPosition: 'start'}});
+    this.shared.openConfirmDialog({
+      title: 'Delete data?',
+      msg: 'Once deleted, all data will be lost!'
+    }).afterClosed().subscribe(result => {
+      if (result === 'ok') {
+        this.shared.openSnackBar({
+          msg: 'We\'re currently deleting your data. This may take a while.',
+          additionalOpts: {
+            duration: null
+          }
+        });
         this.afFs.doc(`users/${this.user.uid}`)
           .delete()
           .then(() => {
-            this.shared.openSnackBar({ msg: 'Data was successfully deleted', additionalOpts: { duration: 4000, horizontalPosition: 'start' } });
+            this.shared.openSnackBar({ msg: 'Data was successfully deleted' });
           })
           .catch((error) => {
             this.handleError(error.message);
           });
       }
-    })
+    });
   }
   /**
    * Deletes the currently logged-in user
-   * @param {boolean} [bypassUnregisterDialog] Whether to skip the unregister confirmation dialog
    */
-  deleteUser(bypassUnregisterDialog?: boolean) {
-    if (!bypassUnregisterDialog) {
-      // tslint:disable-next-line:max-line-length
-      let confirmDialogRef = this.shared.openConfirmDialog({ title: 'Unregister?', msg: '<p>Unregistering will clear all data associated with your account.</p><p><strong>Take note that if you would like to save your data, you can do so by going to Account > Export data.</strong></p>', isHtml: true, ok: 'Unregister and delete data' });
-      confirmDialogRef.afterClosed().subscribe(result => {
-        if (result === 'ok') {
-          // tslint:disable-next-line:max-line-length
-          let doubleConfirmDialogRef = this.shared.openConfirmDialog({ title: 'Really unregister?', msg: 'If you did not export your data, all of your data will be deleted! Continue?', ok: 'Unregister and delete data', okColor: 'warn', hasCheckbox: true, checkboxLabel: 'I confirm that I have backed up all data and that deleting my account will remove all data associated with my account.', checkboxColor: 'primary', dialogRequiresCheckbox: true });
-          doubleConfirmDialogRef.afterClosed().subscribe(result2 => {
-            console.log(result2);
-            console.log(this.user);
-            if (result2 === 'ok' && this.user) {
-              this.deleteData();
-              this.user.delete().then(() => {
-                console.log('User successfully deleted!');
-                // tslint:disable-next-line:max-line-length
-                this.shared.openSnackBar({ msg: 'User successfully deleted from database', additionalOpts: { duration: 4000, panelClass: 'mat-elevation-z3', horizontalPosition: 'start' } });
-              }).catch((error) => {
-                if (error === 'auth/requires-recent-login') {
-                  // User has logged in for a while and hasn't reauthenticated. Firebase needs the user to be recently logged in in order for this to work.
-                  this.user.reauthenticateWithPopup(new firebase.auth.GoogleAuthProvider()).then(() => {
-                    // Attempt deletion again and bypass the confirm dialog
-                    this.deleteUser(true);
-                  })
-                    .catch((error2) => {
-                      this.handleError(error2.message);
-                    });
-                }
-                this.handleError(error.message);
-              });
-            }
+  deleteUser() {
+    const confirmDialogRef = this.shared.openConfirmDialog({
+      title: 'Unregister?',
+      msg: `<p>Unregistering will clear all data associated with your account.</p>
+      <p><strong>Take note that if you would like to save your data, you can do so by going to Account > Export data.</strong></p>`,
+      isHtml: true,
+      ok: 'Unregister and delete data'
+    });
+    confirmDialogRef.afterClosed().subscribe(result => {
+      if (result === 'ok' && this.user) {
+        this.deleteData();
+        this.user.delete().then(() => {
+          console.log('User successfully deleted!');
+          this.shared.openSnackBar({
+            msg: 'Successfully unregistered!'
           });
-        }
-      });
-    } else {
-      this.deleteData();
-      this.user.delete().then(() => {
-        console.log('User successfully deleted!');
-        // tslint:disable-next-line:max-line-length
-        this.shared.openSnackBar({ msg: 'User successfully deleted from database', additionalOpts: { duration: 4000, panelClass: 'mat-elevation-z3', horizontalPosition: 'start' } });
-      }).catch((error) => {
-        if (error === 'auth/requires-recent-login') {
-          // User has logged in for a while and hasn't reauthenticated. Firebase needs the user to be recently logged in in order for this to work.
-          this.user.reauthenticateWithPopup(new firebase.auth.GoogleAuthProvider()).then(() => {
-            // Attempt deletion again and bypass the confirmation dialog
-            this.deleteUser(true);
-          })
-            .catch((error2) => {
-              this.handleError(error2.message);
+        }).catch((error) => {
+          console.error(error);
+          if (error === 'auth/requires-recent-login') {
+            const snackBarRef = this.shared.openSnackBar({
+              msg: 'Please relogin before unregistering first.',
+              action: 'Relogin'
             });
-        }
-        this.handleError(error.message);
-      });
-    }
+            snackBarRef.onAction().subscribe(_ => {
+              // User has not logged in for a while.
+              // Firebase auth needs the user to have a recent login in order for this to work.
+              this.user.reauthenticateWithPopup(new firebase.auth.GoogleAuthProvider()).then(() => {
+                this.deleteUser();
+              })
+                .catch((snackBarError) => {
+                  console.error(snackBarError);
+                  this.handleError(snackBarError.message);
+                });
+            });
+          }
+          this.handleError(error.message);
+        });
+      }
+    });
   }
   private handleError(errorMsg: string, logError: boolean = true) {
     if (logError) {
