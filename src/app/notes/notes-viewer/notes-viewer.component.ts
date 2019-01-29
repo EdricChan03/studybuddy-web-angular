72d1d5c53a5aefba38d5647eebce1c20797f2126
Edric Chan <edric.chan.1997@gmail.com>
2019-01-29 21:31:22 +0800

    fix(notes-viewer): add missing semicolon

--- src/app/notes/notes-viewer/notes-viewer.component.ts
+++ src/app/notes/notes-viewer/notes-viewer.component.ts
@@ -12,7 +12,7 @@ export class NotesViewerComponent implements OnInit {
   constructor(private route: ActivatedRoute) {
     route.params.subscribe(result => {
       console.log(`Route params: ${result}`);
-    })
+    });
   }
 
   ngOnInit() {
