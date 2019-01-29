1d3e03b27399e32f028bd35b0b0596fdc6d6c979
Edric Chan <edric.chan.1997@gmail.com>
2019-01-29 21:42:43 +0800

    chore(dialogs/index): update exports

--- src/app/dialogs/index.ts
+++ src/app/dialogs/index.ts
@@ -1,4 +1,7 @@
+export * from './chat-info-dialog/chat-info-dialog.component';
 export * from './edit-content-dialog/edit-content-dialog.component';
+export * from './join-chat-dialog/join-chat-dialog.component';
+export * from './new-chat-dialog/new-chat-dialog.component';
 export * from './new-project-dialog/new-project-dialog.component';
 export * from './sign-in-dialog/sign-in-dialog.component';
 export * from './todo-dialog/todo-dialog.component';
