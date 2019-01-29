245638129b30275a993958b7c61e39add4c66cb2
Edric Chan <edric.chan.1997@gmail.com>
2019-01-29 21:43:16 +0800

    feat(markdown-dialog): initial dialog to show processed-Markdown text

---
+++ src/app/dialogs/markdown-dialog/markdown-dialog.component.ts
@@ -0,0 +1,52 @@
+import { Component, Inject } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface MarkdownDialogConfig {
+  /**
+   * The dialog's title
+   */
+  title: string;
+  /**
+   * The markdown to display in the dialog
+   */
+  text: string;
+  /**
+   * The colour/color of the negative dialog button
+   */
+  negativeDialogBtnColor?: ThemePalette;
+  /**
+   * Whether to show the negative dialog button
+   */
+  hasNegativeDialogBtn?: boolean;
+  /**
+   * The text of the negative dialog button
+   *
+   * Note: If this isn't set, the button's text will be set to 'Cancel'
+   */
+  negativeDialogBtnText?: string;
+  /**
+   * The colour/color of the positive dialog button
+   */
+  positiveDialogBtnColor?: ThemePalette;
+  /**
+   * Whether to show the positive dialog button
+   */
+  hasPositiveDialogBtn?: boolean;
+  /**
+   * The text of the positive dialog button
+   *
+   * Note: If this isn't set, the button's text will be set to 'Ok'
+   */
+  positiveDialogBtnText?: string;
+}
+
+@Component({
+  selector: 'app-markdown-dialog',
+  templateUrl: './markdown-dialog.component.html'
+})
+export class MarkdownDialogComponent {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: MarkdownDialogConfig) { }
+
+}
