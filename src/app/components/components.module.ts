5accefbdadaa6e6d21c7a4d4a3e77343c7f5d739
Edric <edric.chan.1997@gmail.com>
2019-12-19 10:59:34 +0800

    chore(components): add empty-state module to component module exports

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { EmptyStateModule } from './empty-state/empty-state.module';
 import { ToolbarModule } from './toolbar/toolbar.module';
 
 @NgModule({
   exports: [
+    EmptyStateModule,
     ToolbarModule
   ]
 })
