bc41b11250403e528ad2702038c9f1fcb60585b1
Edric Chan <edric.chan.1997@gmail.com>
2020-11-02 14:57:20 +0800

    fix(todo/todo-project): use RxJS `map` operator instead of `switchMap`

--- src/app/todo/todo-project/todo-project.component.ts
+++ src/app/todo/todo-project/todo-project.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestoreDocument, AngularFirestore } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
-import { filter, switchMap } from 'rxjs/operators';
-import { TodoProject } from '../../interfaces';
 import { Observable } from 'rxjs';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { filter, map } from 'rxjs/operators';
+
+import { TodoItem, TodoProject } from '../../interfaces';
 
 @Component({
   selector: 'app-todo-project',
@@ -16,9 +17,9 @@ export class TodoProjectComponent {
   projectId: string;
   percentageComplete: Observable<number>;
   constructor(
-    private route: ActivatedRoute,
-    private afFs: AngularFirestore,
-    private afAuth: AngularFireAuth
+    route: ActivatedRoute,
+    afFs: AngularFirestore,
+    afAuth: AngularFireAuth
   ) {
     route.params
       .pipe(
@@ -27,14 +28,15 @@ export class TodoProjectComponent {
       .subscribe(params => {
         this.projectId = params.projectId;
       });
+
     afAuth.onAuthStateChanged((user) => {
       if (user) {
-        this.projectDoc = afFs.doc(`users/${user.uid}/todoProjects/${this.projectId}`);
+        this.projectDoc = afFs.doc<TodoProject>(`users/${user.uid}/todoProjects/${this.projectId}`);
         this.project = this.projectDoc.valueChanges();
         this.percentageComplete = this.projectDoc.get()
         .pipe(
-          switchMap((snapshot) => {
-            const val = snapshot.get('todosDone');
+          map(snapshot => {
+            const val = snapshot.get('todosDone') as TodoItem[];
             return val ? val.length : 0;
           })
         );
